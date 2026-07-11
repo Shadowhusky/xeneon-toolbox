b688da30086bf123e79d20c09be4ede6c0d49aa4
shadowhusky <a983974247@gmail.com>
2026-07-11 17:15:07 +0100

    chore: bump version to 1.16.0

--- scripts/make-app.sh
+++ scripts/make-app.sh
@@ -37,8 +37,8 @@ cat > "$APP/Contents/Info.plist" <<PLIST
     <key>CFBundleExecutable</key><string>$BIN_NAME</string>
     <key>CFBundleIconFile</key><string>AppIcon</string>
     <key>CFBundlePackageType</key><string>APPL</string>
-    <key>CFBundleShortVersionString</key><string>1.15.0</string>
-    <key>CFBundleVersion</key><string>18</string>
+    <key>CFBundleShortVersionString</key><string>1.16.0</string>
+    <key>CFBundleVersion</key><string>19</string>
     <key>LSMinimumSystemVersion</key><string>14.0</string>
     <key>NSPrincipalClass</key><string>NSApplication</string>
     <key>NSHighResolutionCapable</key><true/>
